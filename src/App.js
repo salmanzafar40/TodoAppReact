9d65ff91bd807cdff4e9f1c05933dd21100596c9
Salman Zafar <salman.zafar@innowi.com>
2020-08-30 00:36:44 +0500

    Form component done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
         <header>
-            <h1>Ed's Todo List</h1>
+            <h1>Salman's Todo List</h1>
         </header>
+        <Form/>
     </div>
   );
 }
