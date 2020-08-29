9cc603f0f7a380a1a2a4fcda92f27dfe4b88afda
Salman Zafar <salman.zafar@innowi.com>
2020-08-30 01:57:59 +0500

    done with the mark todo as complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,12 +4,29 @@ const Todo = ({todo, todos, setTodos}) => {
     const deleteHandler = () => {
         setTodos(todos.filter(el =>  el.id !== todo.id))
     }
+
+    const completeHandler = (id) => {
+        setTodos(
+            todos.map(item => {
+                if (item.id === id)
+                {
+                    return {
+                        ...item,
+                        completed: !item.completed
+                    }
+                }
+                return item;
+            })
+        )
+    }
+
     return (
         <div className={'todo'}>
             <li className={'todo-item'}>
                 {todo.text}
             </li>
-            <button className={'complete-btn'}>
+            <button onClick={() => completeHandler(todo.id)}
+                    className={`complete-btn ${todo.completed ? 'completed' : ''}`}>
                 <i className="fas fa-check"/>
             </button>
             <button onClick={deleteHandler} className={'trash-btn'}>
